f2881185904220fd187400f92f37197f32971a1f
Nathan Oliveira <52627147+nathan-oliveira@users.noreply.github.com>
2020-06-03 15:19:38 -0300

    TodoApp

---
+++ frontend/src/template/pageHeader.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default props => (
+  <div className="page-header">
+    <h3><small>{props.name} / {props.small}</small></h3>
+  </div>
+)
+
+
---
+++ frontend/src/todo/todoList.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+import IconButton from '../template/iconButton'
+import { markAsDone, markAsPending, remove } from './todoActions'
+
+const TodoList = props => {
+
+  const renderRows = () => {
+    const list = props.list || []
+
+    return list.map(todo => (
+      <tr key={todo._id}>
+        <td className={todo.done ? 'markedAsDone': ''}>{todo.description}</td>
+        <td>
+          <IconButton hide={todo.done} style='success'
+            icon='check' onClick={() => props.markAsDone(todo)}
+          ></IconButton>
+          <IconButton hide={!todo.done} style='warning'
+            icon='undo' onClick={() => props.markAsPending(todo)}
+          ></IconButton>
+          <IconButton hide={!todo.done} style='danger'
+            icon='trash-o' onClick={() => props.remove(todo)}
+          ></IconButton>
+        </td>
+      </tr>
+    ))
+  }
+
+  return (
+    <table className='table table-bordered'>
+      <thead>
+        <tr>
+          <th>Descrição</th>
+          <th className='tableActions'>Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        {renderRows()}
+      </tbody>
+    </table>
+  )
+}
+
+const mapStateToProps = state => ({
+  list: state.todo.list
+})
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  markAsDone,
+  markAsPending,
+  remove
+}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
